9069c35affe8d87c225c286bd439229a27ee779d
BrendanMolloy <brendanmolloyonline@gmail.com>
2019-11-08 11:31:20 +0000

    created cards of potatoes.html

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from env import MONGO_URI
 
-
-
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'tater-rater'
-app.config["MONGO_URI"] = MONGO_URI
+app.config["MONGO_URI"] = (MONGO_URI)
 
 mongo = PyMongo(app)
 
@@ -16,6 +14,17 @@ mongo = PyMongo(app)
 @app.route('/get_potatoes')
 def get_potatoes():
     return render_template("potatoes.html", potatoes=mongo.db.potatoes.find())
+
+@app.route('/add_potato')
+def add_potato():
+    return render_template("submit.html")
+
+@app.route('/submit_potato', methods=['POST'])
+def submit_potato():
+    potatoes=mongo.db.potatoes
+    potatoes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_potatoes'))
+
 
 
 if __name__ == '__main__':
